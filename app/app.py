05e9d601fac94f4bc29e3a7db8fa48301ca59164
Syed Abbas <sanaqvi573@gmail.com>
2026-02-17 17:30:40 +0000

    Update app.py to verify webhook working

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ def health():
 
 @app.get("/")
 def root():
-    return "Let the fun begin:)\n"
+    return "Let the fun continue with DevOps:)\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
