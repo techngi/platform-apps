2d8dd22f0c2aa15d0455d62bd4749b14a7cf0b20
Syed Abbas <sanaqvi573@gmail.com>
2026-04-01 18:15:02 +0000

    Added the applicaton metrix

--- app/app.py
+++ app/app.py
@@ -1,15 +1,18 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
-@app.get("/health")
-def health():
-    return jsonify(app="DevOps-app", status="ok", version=os.getenv("APP_VERSION", "dev"))
+REQUESTS = Counter('http_requests_total', 'Total HTTP Requests')
 
-@app.get("/")
-def root():
-    return "Welcome to the world of DevOps - Stage2:)\n"
+@app.route('/')
+def home():
+    REQUESTS.inc()
+    return "Welcome to the world of DevOps - Stage2 :)"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
