1553528a242e7fd483c1d88ed92bdb5695f74c04
Syed Abbas <sanaqvi573@gmail.com>
2026-04-09 18:02:41 +0000

    Application update to trigger app

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ def health():
 @app.get("/")
 def root():
     REQUESTS.inc()
-    return "Welcome to the world of DevOps - Stage2:)\n"
+    return "Welcome to the world of DevOps - StageFull:)\n"
 
 @app.get("/metrics")
 def metrics():
